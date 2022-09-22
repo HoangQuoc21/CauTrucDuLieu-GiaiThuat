75b16b3a3bbee650a6fbe9a8a320c470009a2455
HoangQuoc21 <hoangquoc2106@gmail.com>
2022-09-22 18:31:25 +0700

    22/09/2022

--- NganXep(Stack)/Polish_notation.cpp
+++ NganXep(Stack)/Polish_notation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 //KÝ PHÁP BALAN CHUYỂN TỪ TRUNG TỐ SANG HẬU TỐ
@@ -51,22 +52,25 @@ char Pop(Stack &stack){
     }
 }
 
-//Kiểm tra là toán hạng
-bool isOperand(char character){
-    if (character <= 'Z' && character >= 'A')
-        return true;
-    return false;
+//Kiểm tra ký tự là toán hạng chữ
+bool isLetterOperand(char character){
+    return ((character <= 'z' && character >= 'a')||(character <= 'Z' && character >= 'A'));
 }
 
-//Kiểm tra là toán tử
+//Kiểm tra ký tự là toán hạng số
+bool isNumberOperand(char character){
+    return (character <= '9' && character >= '0');
+}
+
+//Kiểm tra ký tự là toán tử chữ
 bool isOperator(char character){
-    if (character == '*' || character == '/' || character == '+' || character == '-' || character == '&' || character == '^')
-        return true;
-    return false;
+    return (character == '*' || character == '/' || character == '+' || character == '-' || character == '&' || character == '^');
 }
 
 int getPriority(char c){
-    if (c == '*' || c == '/' || c == '%')
+    if (c == '^')
+        return 3;
+    else if (c == '*' || c == '/' || c == '%')
         return 2;
     else if (c == '+' || c == '-')
         return 1;
@@ -75,51 +79,98 @@ int getPriority(char c){
         return 0;
 }
 
+string infixTopostfix(string infix_notation){
+    string postfix_notation;
+    Stack stack;
+    initStack(stack);
+    //Bắt đầu chuyển từ trung tố sang hậu tố: 
+    //0.1
+    Push(stack, '(');
+    //0.2
+    infix_notation.push_back(')');
+    for (int i = 0; i < infix_notation.size(); i++){
+        //1.
+        char x = infix_notation[i];
+        //2.
+        if (isLetterOperand(x) || isNumberOperand(x))
+            postfix_notation.push_back(x);
+        //3.
+        if (x == '(')
+            Push(stack,x);
+        //4.
+        if (isOperator(x)){
+            while(!isEmpty(stack) && (getPriority(stack.pTop->data) >= getPriority(x)) && isOperator(stack.pTop->data)){
+                char w = Pop(stack);
+                postfix_notation.push_back(w);
+            }
+            Push(stack,x);
+        }
+        //5.
+        if (x == ')'){
+            while(!isEmpty(stack) && (stack.pTop->data != '(')){
+                char w = Pop(stack);
+                postfix_notation.push_back(w);
+            }
+            Pop(stack);
+        }
+    }
+    return postfix_notation;
+}
+
+int cToi(char c){
+    return (int)(c - 48);
+}
+
+char iToc(int i){
+   return (char)(i + 48);
+}
+
+//Tính giá trị biểu thức hậu tố
+int EvaluatePostfix(string infix_notation){
+    string postfix_notation = infixTopostfix(infix_notation);
+    Stack stack;
+    initStack(stack);
+    //Duyệt hết qua các phần tử của biểu thức hậu tố từ trái sang phải
+    for (int i=0; i< postfix_notation.size(); i++){
+        char c = postfix_notation[i];
+        //Nếu là toán hạng số thì Push vào stack
+        if (isNumberOperand(c))
+            Push(stack,c);
+        //Nếu là toán tử thì tính giá trị biểu thức tương ứng với 2 toán hạng ở đỉnh stack
+        if (isOperator(c)){
+            int x = cToi(Pop(stack));
+            int y = cToi(Pop(stack));
+            switch(c){
+                case '+': y = y+x; break;
+                case '-': y = y-x; break;
+                case '*': y = y*x; break;
+                case '/': y = y/x; break;
+                case '^': y = pow(y,x); break;
+                case '%': y = y%x; break;
+            }
+        //Sau khi tính xong thì Push kết quả vừa rồi vào stack
+            char z = iToc(y);
+            Push(stack,z);
+        }
+    }
+    //Sau khi duyệt hết biểu thức hậu tố và tính toán thì kết quả của biểu thức hậu tố chính là toán hạng cuối cùng còn trong stack
+    return cToi(Pop(stack));
+}
+
+
 int main(){
     while(1){
-        system("cls");
         string infix_notation, postfix_notation;
-        cout << "Hay nhap vao bieu thuc trung to ban dau: ";
+        system("cls");
+        cout << "==============================KY PHAP BA LAN=============================\n";
+        cout << "Dau tien hay nhap vao bieu thuc trung to: ";
         cin >> infix_notation;
-
-        //Bắt đầu chuyển từ trung tố sang hậu tố: 
-        Stack stack;
-        initStack(stack);
-        
-        //0.1
-        Push(stack, '(');
-        //0.2
-        infix_notation.push_back(')');
-        for (int i = 0; i < infix_notation.size(); i++){
-            //1.
-            char x = infix_notation[i];
-            //2.
-            if (isOperand(x))
-                postfix_notation.push_back(x);
-            //3.
-            if (x == '(')
-                Push(stack,x);
-            //4.
-            if (isOperator(x)){
-                while(!isEmpty(stack) && (getPriority(stack.pTop->data) >= getPriority(x)) && isOperator(stack.pTop->data)){
-                    char w = Pop(stack);
-                    postfix_notation.push_back(w);
-                }
-                Push(stack,x);
-            }
-            //5.
-            if (x == ')'){
-                while(!isEmpty(stack) && (stack.pTop->data != '(')){
-                    char w = Pop(stack);
-                    postfix_notation.push_back(w);
-                }
-                Pop(stack);
-            }
-        }
-            cout << "Dang hau to cua trung to " << infix_notation << "la: ";
-            cout << postfix_notation << endl;
-            system("pause");
-        }
-   
+        postfix_notation = infixTopostfix(infix_notation);
+        cout << "-Bieu thuc hau to cua no la: ";
+        cout << postfix_notation << endl;
+        cout << "-Gia tri cua bieu thuc hau to tren la (chi dung khi cac toan hang nhap vao la so): ";
+        cout << EvaluatePostfix(infix_notation) << endl;
+        system("pause");
+    }
     return 0;
 }
