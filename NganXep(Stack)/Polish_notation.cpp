f5423b895a0831f545018bbd3defc5a4e172f440
HoangQuoc21 <hoangquoc2106@gmail.com>
2022-09-23 08:21:38 +0700

    23/09/2022

--- NganXep(Stack)/Polish_notation.cpp
+++ NganXep(Stack)/Polish_notation.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 //KÝ PHÁP BALAN CHUYỂN TỪ TRUNG TỐ SANG HẬU TỐ
+//VÀ CHUYỂN TỪ TRUNG TỐ SANG TIỀN TỐ
 
 struct Node{
     char data;
@@ -62,63 +63,81 @@ bool isNumberOperand(char character){
     return (character <= '9' && character >= '0');
 }
 
-//Kiểm tra ký tự là toán tử
+//Kiểm tra ký tự là toán tử chữ
 bool isOperator(char character){
     return (character == '*' || character == '/' || character == '+' || character == '-' || character == '&' || character == '^');
 }
 
 int getPriority(char c){
     if (c == '^')
+        return 4;
+    else if (c == '*')
         return 3;
-    else if (c == '*' || c == '/' || c == '%')
+    else if (c == '/' || c == '%')
         return 2;
     else if (c == '+' || c == '-')
         return 1;
     else
-        // phép ^ có ưu tiên thấp hơn tất cả?
         return 0;
 }
 
-string infixTopostfix(string infix_notation){
+string infixToPostfix(string infix_notation){
     string postfix_notation;
     Stack stack;
     initStack(stack);
     //Bắt đầu chuyển từ trung tố sang hậu tố: 
-    //0.1 Thêm '(' vào stack
+    //0.1
     Push(stack, '(');
-    //0.2 thêm ')' vào biểu thức trung tố
+    //0.2
     infix_notation.push_back(')');
     for (int i = 0; i < infix_notation.size(); i++){
-        //1. Duyệt từng ký tự từ trái sang phải
+        //1.
         char x = infix_notation[i];
-        //2. Nếu là toán hạng thì Push vào biểu thức hậu tố
+        //2.
         if (isLetterOperand(x) || isNumberOperand(x))
             postfix_notation.push_back(x);
-        //3. Nếu là dấu '(' thì Push vào stack
+        //3.
         if (x == '(')
             Push(stack,x);
-        //4. Nếu là toán tử thì 
+        //4.
         if (isOperator(x)){
-            //4.1 Nếu trên đỉnh stack là toán tử và toán tử đó có độ ưu tiên lớn hơn toán tử đang xét thì
             while(!isEmpty(stack) && (getPriority(stack.pTop->data) >= getPriority(x)) && isOperator(stack.pTop->data)){
-                //Push toán tử đó vào biểu thức hậu tố
                 char w = Pop(stack);
                 postfix_notation.push_back(w);
             }
-            //4.2 Push toán tử đang xét vào stack
             Push(stack,x);
         }
-        //5. Nếu là dấu ')' thì Pop hết các ký tự trong stack ra đồng thời Push các ký tự đó vào biểu thức hậu tố. Dừng lại khi gặp dấu ')' trong stack 
+        //5.
         if (x == ')'){
             while(!isEmpty(stack) && (stack.pTop->data != '(')){
                 char w = Pop(stack);
                 postfix_notation.push_back(w);
             }
-            //Pop dấu '(' được bỏ vào stack ở bước 0.1 ra
             Pop(stack);
         }
     }
     return postfix_notation;
+}
+
+//Đảo ngược biểu thức hậu tố sẽ được biểu thức tiền tố
+string infixToPrefix(string infix_notation){
+    string prefix_notation, postfix_notation, new_infix_notation;
+    //Viết lại "Biểu Thức Trung Tố Mới" bằng cách đảo ngược biểu thức trung tố đã cho
+    for (int i = infix_notation.size(), j = 0; i >= 0;i--){
+        //Chú ý dấu '(' phải đổi thành ')' và ngược lại
+        if (infix_notation[i] == '(')
+            new_infix_notation.push_back(')');
+        else if (infix_notation[i] == ')')
+            new_infix_notation.push_back('(');
+        else
+            new_infix_notation.push_back(infix_notation[i]) ;
+    }
+    //Tính biểu thức hậu tố của Biểu Thức Trung Tố Mới
+    postfix_notation = infixToPostfix(new_infix_notation);
+    //Nghịch đảo biểu thức hậu tố mới tính sẽ được biểu thức tiền tố
+    for (int i = postfix_notation.size(); i >= 0; i--)
+        prefix_notation.push_back(postfix_notation[i]);
+    return prefix_notation;
 }
 
 int cToi(char c){
@@ -131,7 +150,7 @@ char iToc(int i){
 
 //Tính giá trị biểu thức hậu tố
 int EvaluatePostfix(string infix_notation){
-    string postfix_notation = infixTopostfix(infix_notation);
+    string postfix_notation = infixToPostfix(infix_notation);
     Stack stack;
     initStack(stack);
     //Duyệt hết qua các phần tử của biểu thức hậu tố từ trái sang phải
@@ -163,17 +182,45 @@ int EvaluatePostfix(string infix_notation){
 
 
 int main(){
-    while(1){
-        string infix_notation, postfix_notation;
+    system("cls");
+    string infix_notation, choice;
+    cout << "==============================KY PHAP BA LAN===========================\n";
+    cout << "Hay nhap vao Bieu Thuc Trung To cua ban: ";
+    cin >> infix_notation;
+    while (choice != "0"){
         system("cls");
-        cout << "==============================KY PHAP BA LAN=============================\n";
-        cout << "Dau tien hay nhap vao bieu thuc trung to: ";
-        cin >> infix_notation;
-        postfix_notation = infixTopostfix(infix_notation);
-        cout << "-Bieu thuc hau to cua no la: ";
-        cout << postfix_notation << endl;
-        cout << "-Gia tri cua bieu thuc hau to tren la (chi dung khi cac toan hang nhap vao la so): ";
-        cout << EvaluatePostfix(infix_notation) << endl;
+        cout << "============================== KY PHAP BA LAN ===========================\n";
+        cout << "Bieu Thuc Trung To ban da nhap: " << infix_notation << endl;
+        cout << "================================== MENU =================================\n";
+        cout << "1. Chuyen sang Bieu Thuc Tien To va tinh gia tri cua no.\n";
+        cout << "2. Chuyen sang Bieu thuc Hau To va tinh gia tri cua no.\n";
+        cout << "3. Nhap lai Bieu Thuc Trung To.\n";
+        cout << "0. Thoat chuong trinh.\n";
+        cout << "=================================== END =================================\n";
+        cout << "Lua chon cua ban la: ";
+        cin >> choice;
+        cout << "-------------------------------------------------------------------------\n";
+        if (choice == "1"){
+            string prefix_notation = infixToPrefix(infix_notation);
+            cout << "-Bieu Thuc Tien To: " << prefix_notation << endl;
+            cout << "-Gia tri cua Bieu Thuc Tien To (Chi dung khi toan hang la so): " << endl;
+        }
+        else if (choice == "2"){
+            string postfix_notation = infixToPostfix(infix_notation);
+            int value = EvaluatePostfix(postfix_notation);
+            cout << "-Bieu Thuc Tien To: " << postfix_notation << endl;
+            cout << "-Gia tri cua Bieu Thuc Tien To (Chi dung khi toan hang la so): " << value << endl;
+        }
+        else if (choice == "3"){
+            cout << "-Hay nhap Bieu Thuc Trung To: ";
+            cin >> infix_notation;
+        }
+        else if (choice == "0"){
+            cout << "Thoat chuong trinh.\n";
+            exit(0);
+        }
+        else 
+            cout << "Lua chon khong hop le.\n";
         system("pause");
     }
     return 0;
